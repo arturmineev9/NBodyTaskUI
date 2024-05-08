afb0d4dd5b3b731d3c2d91956bddb32425f87a30
Arthur <arturmineev9@mail.ru>
2024-05-08 16:16:49 +0300

    Добавление BodiesAcceptableParams
    Класс, в котором находятся допустимые значения для входных данных

---
+++ NBodyTask/NBodyTask/BodiesAcceptableParams.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBodyTask
+{
+    internal class BodiesAcceptableParams
+    {
+        public static readonly int minBodiesNum = 1;
+        public static readonly int maxBodiesNum = 1024;
+
+        public static readonly double minBodyMass = 1e3;
+        public static readonly double maxBodyMass = 9e14;
+
+        public static readonly int MIN_DELTA_TIME = 16;
+        public static readonly int MAX_DELTA_TIME = 1_000;
+
+    }
+}
